0e009a7e378e3ba387bc8599ca68ceb189a03d39
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-05-22 09:26:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let h1 = document.createElement('h1');
+h1.innnerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+
+let div =document.getElementById('sizeInfo');
+div.appendChild(h1);
+
+window.onsize = function() {
+	h1.innnerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+
+}
